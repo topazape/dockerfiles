ebb5fd1ffc2659e799b9e0ca0bc3cdaf12a52b26
topazape <topazape@gmail.com>
2022-04-02 22:40:08 +0900

    files are now owned by user privilege

---
+++ embulk/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export USER=embulk
+export HOME=/home/$USER
+
+uid=$(stat -c "%u" .)
+gid=$(stat -c "%g" .)
+
+if [ ${uid} -ne 0 ]; then
+	if [ $(id -g ${USER}) -ne ${gid} ]; then
+		getent group ${gid} > /dev/null @>&1 || groupmod -g ${gid} ${USER}
+		chgrp -R ${gid} ${HOME}
+	fi
+	if [ $(id -u ${USER}) -ne ${uid} ]; then
+		usermod -u ${uid} ${USER}
+	fi
+fi
+
+exec setpriv --reuid=${USER} --regid=${USER} --init-groups "$@"
